fb41360d59cbcff8c69f387ebffc1f7f9ada4133
Gero Zayas <gerozayas@gmail.com>
2024-05-07 07:06:39 +0200

    Create div

--- main.py
+++ main.py
@@ -7,18 +7,22 @@
 
 from nicegui import ui
 
-app_name_label_mk = "# Problem Solving Questions"
-tony_robbins_mk = "### by Tony Robbins"
-
-ui.markdown(app_name_label_mk).classes(
-    "h-60 w-60 bg-gradient-to-r from-red-100 to-blue-300 p-5"
-)
-ui.markdown(tony_robbins_mk)
-ui.image(
-    "https://www.usmagazine.com/wp-content/uploads/2019/04/Tony-Robbins-25-Things-You-Dont-Know-About-Me.jpg?quality=47&strip=all"
-).classes("w-32")
+app_name_label = "# Problem Solving Questions"
+tony_robbins = "### by Tony Robbins"
+gero_zayas_link = "Made with <3 by Gero Zayas"
+gero_zayas_url = "https://www.gerozayas.com"
 
 
-ui.link("Made with <3 by Gero Zayas", "https://www.gerozayas.com")
+with ui.element("div").classes("p-1 bg-blue-100"):
+    ui.label("inside a colored div")
+    ui.markdown(app_name_label).classes(
+        "h-60 w-60 bg-gradient-to-r from-red-100 to-blue-300 p-5"
+    )
+    ui.markdown(tony_robbins)
+    ui.image(
+        "https://www.usmagazine.com/wp-content/uploads/2019/04/Tony-Robbins-25-Things-You-Dont-Know-About-Me.jpg?quality=47&strip=all"
+    ).classes("w-32")
+
+    ui.link(gero_zayas_link, gero_zayas_url)
 
 ui.run()
